206ec3e935da583b27a6b62e1729f38737ec6d7e
Freeman Liu <llw599502537@gmail.com>
2025-08-10 21:24:25 +0800

    keep going

--- services/user/src/main/java/com/example/user/grpc/UserGrpcService.java
+++ services/user/src/main/java/com/example/user/grpc/UserGrpcService.java
@@ -3,12 +3,8 @@ package com.example.user.grpc;
 import com.example.user.GetUserRequest;
 import com.example.user.GetUserResponse;
 import com.example.user.UserServiceGrpc;
-import com.example.user.service.UserService;
 import io.grpc.stub.StreamObserver;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * gRPC Service implementation for User operations
@@ -16,16 +12,6 @@ import org.slf4j.LoggerFactory;
  */
 @Component
 public class UserGrpcService extends UserServiceGrpc.UserServiceImplBase {
-    
-    private static final Logger logger = LoggerFactory.getLogger(UserGrpcService.class);
-    
-    @Autowired
-    private UserService userService;
-    
-    // TODO: Implement gRPC methods when proto files are generated
-    public void placeholder() {
-        logger.info("UserGrpcService placeholder - gRPC implementation will be added when proto files are ready");
-    }
 
     @Override
     public void getUser(GetUserRequest request, StreamObserver<GetUserResponse> responseObserver) {
--- services/user/src/test/java/com/example/user/UserAppIT.java
+++ services/user/src/test/java/com/example/user/UserAppIT.java
@@ -1,13 +1,21 @@
 
 package com.example.user;
 
+import grpcstarter.server.GrpcServer;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class UserAppIT {
 
+    @Autowired
+    GrpcServer grpcServer;
+
     @Test
     void contextLoads() {
+        assertThat(grpcServer).isNotNull();
     }
 }
